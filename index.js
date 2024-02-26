2227c94387fe9bd6d4cd3728e1d0e1911c1104e8
mohammad shoaib <mohammadshoaib7221@gmail.com>
2024-02-26 14:52:12 +0530

    addTodo functionality added

--- index.js
+++ index.js
@@ -1,19 +1,67 @@
-const express = require( 'express' );
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const bodyParser = require("body-parser");
 
 const PORT = 8000;
+const app = express();
 
-const app =  express();
+const connectionUrl = "mongodb://localhost:27017/todoDb";
 
-//view engine
+mongoose
+  .connect(connectionUrl)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+const todoSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true,unique:true },
+    desc: { type: String },
+  },
+  { timestamps: true }
+);
+
+const Todo = mongoose.model("Todo", todoSchema);
 
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-//static files
 
-app.use( express.static('public') ) ;
+app.get("/", (req, res, next) => {
+  try {
+    res.render("index", { title: "List Todo" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
-// listen server
+app.get("/add-todo", (req, res, next) => {
+  try {
+    res.render("newTodo", { title: "Add Todo" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+app.post("/add-todo", async (req, res, next) => {
+  try {
+    if(!title){
+      return res.status(400).json({message:"Title is empty!"});
+    }
+    const { title, desc } = req.body;
+    const newTodo = new Todo({ title, desc });
+    await newTodo.save();
+    res.status(201).redirect("/");
+    } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
