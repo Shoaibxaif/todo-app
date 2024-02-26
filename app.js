2f09515da32b0bdc770f5d0d5f3b34f1b3c5908b
mohammad shoaib <mohammadshoaib7221@gmail.com>
2024-02-26 19:35:39 +0530

    code refactor-5

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const MongodbConnection = require("./init/mongodb");
+const todoRoute = require("./routes/todo");
+
+MongodbConnection();
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/", todoRoute);
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,19 +1,6 @@
-const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
-const MongodbConnection = require("./init/mongodb");
-const todoRoute = require("./routes/todo");
-
-MongodbConnection();
+const app = require("./app");
 
 const PORT = 8000;
-const app = express();
-
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/",todoRoute);
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
